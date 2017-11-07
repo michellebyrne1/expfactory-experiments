583338200cba23333f9e5ccddcec24dba6133863
john flournoy <flournoy@uoregon.edu>
2017-11-07 08:00:12 -0800

    deploy yads

---
+++ static/experiments/soc_prob_learning/save_data.php
@@ -0,0 +1,9 @@
+<?php
+// the $_POST[] array will contain the passed in filename and data
+// the directory "data" is writable by the server (chmod 777)
+// CHANGE THIS FILENAME SO THE PATH MAKES SENSE
+$filename = "/experiment_data/yads/".$_POST['filename'];
+$data = $_POST['filedata'];
+// write the file to disk
+file_put_contents($filename, $data);
+?>
---
+++ static/js/load_experiments.js
@@ -0,0 +1,47 @@
+function loadjscssfile(filename, filetype){
+	if (filetype == "js") {
+		document.write('<script src =' + filename + '></script>')
+	}
+	else if (filetype = "css") {
+		document.write('<link href =' + filename + ' rel="stylesheet" type="text/css">')
+	}
+}
+
+
+/* Draws experiments randomly to fill up a certain amount of time. 
+Completely avoids any knapsack type optimization and just stops 
+when it can't find another experiment to add */
+
+var experimentDraw = function(lst, time) {
+	var time = time || "99999"
+	var return_list = []
+	var total_time = 0
+	while (total_time < time && lst.length > 0) {
+		index = Math.floor(Math.random()*lst.length)
+		if ((total_time + lst[index].time) < time) {
+			total_time += lst[index].time
+			return_list.push(lst[index].name)
+		} 
+		lst.splice(index,1)
+	}
+    return return_list
+}
+
+// full list of experiment names:
+experiment_list = [{'name': 'soc_prob_learning', 'time': 14}]						
+experiment_names = experimentDraw(experiment_list)
+
+
+/* takes an experiment array and concatenates it with the array of each experiment \
+identified in 'experiment_names' */
+function cat_experiments(experiment_array) {
+	for (i = 0; i < experiment_names.length; i++) {
+		switch (experiment_names[i]) {
+                
+case "soc_prob_learning":
+      experiments = experiments.concat(soc_prob_learning_experiment)
+      break;
+
+		}
+	}
+}
