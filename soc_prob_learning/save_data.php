7456452c0245fa240cb0122a6828033504fae73c
Michelle Byrne <mbyrne@uoregon.edu>
2018-12-18 20:39:37 -0800

    updated for sos final

--- soc_prob_learning/save_data.php
+++ soc_prob_learning/save_data.php
@@ -2,7 +2,7 @@
 // the $_POST[] array will contain the passed in filename and data
 // the directory "data" is writable by the server (chmod 777)
 // CHANGE THIS FILENAME SO THE PATH MAKES SENSE
-$filename = "/experiment_data/sos_pilot/".$_POST['filename'];
+$filename = "/experiment_data/sos_final/".$_POST['filename'];
 $data = $_POST['filedata'];
 // write the file to disk
 file_put_contents($filename, $data);
